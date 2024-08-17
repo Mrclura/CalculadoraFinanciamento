258359ecce7333d032546a96352b689e7b9e04a6
GabrielaTamura <51144634+GabrielaTamura@users.noreply.github.com>
2024-08-17 00:52:28 -0300

    Added types

--- src/Main.hs
+++ src/Main.hs
@@ -6,41 +6,53 @@ import System.IO (hFlush, stdout)
 import Text.Printf (printf)
 
 type Historico = [Simulacao]
+type ValorObjeto = Double
+type ValorFinanciado = Double
+type ValorEntrada = Double
+type QntdParcelas = Int
+type IdParcela = Int
+type TaxaMensal = Double
+type ValorTotalPago = Double
+type CustoEfetivoTotal = Double
+type ValorParcela = Double
+type ValorAmortizado = Double
+type JurosParcela = Double
+type SaldoDevedor = Double
 
 data Simulacao = Simulacao
-  { valorObjeto        :: Double
-  , entrada            :: Double
-  , parcelas           :: Int
-  , taxaMensal         :: Double
-  , valorFinanciado    :: Double
-  , parcelaMensal      :: Double
-  , custoEfetivoTotal  :: Double
-  , valorTotalPago     :: Double
+  { valorObjeto        :: ValorObjeto
+  , valorEntrada       :: ValorEntrada
+  , qntdParcelas       :: Int
+  , taxaMensal         :: TaxaMensal
+  , valorFinanciado    :: ValorFinanciado
+  , valorParcela       :: ValorParcela
+  , custoEfetivoTotal  :: CustoEfetivoTotal
+  , valorTotalPago     :: ValorTotalPago
   } deriving Show
 
 -- Função para calcular o valor da parcela fixa
-calcularParcelaFixa :: Double -> Double -> Int -> Double
-calcularParcelaFixa valorFinanciado taxaMensal numParcelas =
+calcularParcelaFixa :: ValorFinanciado -> TaxaMensal -> QntdParcelas -> ValorParcela
+calcularParcelaFixa valorFinanciado taxaMensal qntdParcelas =
   let taxa = taxaMensal / 100
-  in (valorFinanciado * taxa * (1 + taxa) ^ fromIntegral numParcelas) / ((1 + taxa) ^ fromIntegral numParcelas - 1)
+  in (valorFinanciado * taxa * (1 + taxa) ^ fromIntegral qntdParcelas) / ((1 + taxa) ^ fromIntegral qntdParcelas - 1)
 
 -- Função para calcular o valor total pago e o custo efetivo total, utilizando foldl
-calcularCETeTotalPago :: Double -> Double -> Int -> (Double, Double)
-calcularCETeTotalPago parcela valorFinanciado numParcelas =
-  let parcelasList = replicate numParcelas parcela
-      valorTotalPago = foldl' (+) 0 parcelasList
+calcularCETeTotalPago :: ValorParcela -> ValorFinanciado -> QntdParcelas -> (ValorTotalPago, CustoEfetivoTotal)
+calcularCETeTotalPago parcela valorFinanciado qntdParcelas =
+  let qntdParcelasList = replicate qntdParcelas parcela
+      valorTotalPago = foldl' (+) 0 qntdParcelasList
       custoEfetivoTotal = ((valorTotalPago - valorFinanciado) / valorFinanciado) * 100
   in (valorTotalPago, custoEfetivoTotal)
 
 -- Função para gerar a tabela de amortização de forma recursiva
-gerarTabelaAmortizacao :: Double -> Double -> Int -> Double -> [(Int, Double, Double, Double, Double)]
-gerarTabelaAmortizacao valorFinanciado taxaMensal numParcelas parcela = gerarParcelas valorFinanciado 1
+gerarTabelaAmortizacao :: ValorFinanciado -> TaxaMensal -> QntdParcelas -> ValorParcela -> [(IdParcela, ValorParcela, JurosParcela, ValorAmortizado, SaldoDevedor)]
+gerarTabelaAmortizacao valorFinanciado taxaMensal qntdParcelas parcela = gerarParcelas valorFinanciado 1
   where
     taxa = taxaMensal / 100
     
-    gerarParcelas :: Double -> Int -> [(Int, Double, Double, Double, Double)]
+    gerarParcelas :: SaldoDevedor -> IdParcela -> [(IdParcela, ValorParcela, JurosParcela, ValorAmortizado, SaldoDevedor)]
     gerarParcelas saldoDevedor i
-      | i > numParcelas = []
+      | i > qntdParcelas = []
       | otherwise =
           let juros = saldoDevedor * taxa
               amortizacao = parcela - juros
@@ -48,12 +60,12 @@ gerarTabelaAmortizacao valorFinanciado taxaMensal numParcelas parcela = gerarPar
           in (i, parcela, juros, amortizacao, saldoDevedorNovo) : gerarParcelas saldoDevedorNovo (i + 1)
 
 -- Adicionar simulação ao histórico
-adicionarSimulacao :: Double -> Double -> Int -> Double -> State Historico Simulacao
-adicionarSimulacao valorObjeto entrada numParcelas taxaMensal = do
-  let valorFinanciado = valorObjeto - entrada
-      parcela = calcularParcelaFixa valorFinanciado taxaMensal numParcelas
-      (valorTotalPago, custoEfetivoTotal) = calcularCETeTotalPago parcela valorFinanciado numParcelas
-      simulacao = Simulacao valorObjeto entrada numParcelas taxaMensal valorFinanciado parcela custoEfetivoTotal valorTotalPago
+adicionarSimulacao :: ValorObjeto -> ValorEntrada -> QntdParcelas -> TaxaMensal -> State Historico Simulacao
+adicionarSimulacao valorObjeto valorEntrada qntdParcelas taxaMensal = do
+  let valorFinanciado = valorObjeto - valorEntrada
+      parcela = calcularParcelaFixa valorFinanciado taxaMensal qntdParcelas
+      (valorTotalPago, custoEfetivoTotal) = calcularCETeTotalPago parcela valorFinanciado qntdParcelas
+      simulacao = Simulacao valorObjeto valorEntrada qntdParcelas taxaMensal valorFinanciado parcela custoEfetivoTotal valorTotalPago
   historico <- get
   put (simulacao : historico)
   return simulacao
@@ -73,18 +85,18 @@ main = do
         putStrLn "Digite o valor do objeto financiado: "
         hFlush stdout
         valorObjeto <- readLn
-        putStrLn "Digite o valor da entrada: "
+        putStrLn "Digite o valor da valorEntrada: "
         hFlush stdout
-        entrada <- readLn
-        putStrLn "Digite o número total de parcelas: "
+        valorEntrada <- readLn
+        putStrLn "Digite o número total de qntdParcelas: "
         hFlush stdout
-        parcelas <- readLn
+        qntdParcelas <- readLn
         putStrLn "Digite a taxa de juros mensal (em porcentagem, por exemplo, 1 para 1%): "
         hFlush stdout
         taxa <- readLn
-        let simulacao = evalState (adicionarSimulacao valorObjeto entrada parcelas taxa) historico
-        let parcela = calcularParcelaFixa (valorObjeto - entrada) taxa parcelas
-        let tabela = gerarTabelaAmortizacao (valorObjeto - entrada) taxa parcelas parcela
+        let simulacao = evalState (adicionarSimulacao valorObjeto valorEntrada qntdParcelas taxa) historico
+        let parcela = calcularParcelaFixa (valorObjeto - valorEntrada) taxa qntdParcelas
+        let tabela = gerarTabelaAmortizacao (valorObjeto - valorEntrada) taxa qntdParcelas parcela
         putStrLn $ "Valor financiado: " ++ formatarDouble (valorFinanciado simulacao)
         putStrLn $ "Parcela mensal: " ++ formatarDouble parcela
         putStrLn $ "Custo efetivo total (CET): " ++ formatarDouble (custoEfetivoTotal simulacao) ++ "%"
